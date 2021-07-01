fae41bd09b1afdedfeee54de2ab5ed7bb04bc2f8
김종환 <dondon9513@gmail.com>
2021-07-01 13:44:35 +0900

    add baseComponent

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,16 +1,8 @@
-export class PageComponent {
-  private element: HTMLUListElement;
-  constructor() {
-    /**
-     * <ul class="page">This is PageComponent</ul>
-     */
-    this.element = document.createElement('ul');
-    this.element.setAttribute('class', 'page');
-    this.element.textContent = 'This is PageComponent';
-  }
+import { BaseComponent } from '../component.js';
 
-  attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
-    // element를 해당하는 position에 추가
-    parent.insertAdjacentElement(position, this.element);
+export class PageComponent extends BaseComponent<HTMLUListElement> {
+
+  constructor() {
+    super('<ul class="page">This is PageComponent!</ul>')
   }
 }
