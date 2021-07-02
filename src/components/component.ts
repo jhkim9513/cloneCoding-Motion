e0a48ece93351b5926d364dbca60610289b027ce
김종환 <dondon9513@gmail.com>
2021-07-02 19:21:19 +0900

    added item remove function

--- src/components/component.ts
+++ src/components/component.ts
@@ -1,5 +1,6 @@
 export interface Component {
   attachTo(parent: HTMLElement, position?: InsertPosition): void;
+  removeFrom(parent: HTMLElement): void;
 }
 /**
  * Encapsulate the HTMl element creation
@@ -16,5 +17,13 @@ export class BaseComponent<T extends HTMLElement> implements Component {
   attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
     // element를 해당하는 position에 추가
     parent.insertAdjacentElement(position, this.element);
+  }
+
+  removeFrom(parent: HTMLElement) {
+    if (parent !== this.element.parentElement) {
+      throw new Error('Parent mismatcch!');
+    }
+    // element를 삭제
+    parent.removeChild(this.element);
   }
 }
--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -2,7 +2,10 @@ import { BaseComponent, Component } from '../component.js';
 export interface Composable {
   addChild(child: Component): void;
 }
+type OnCloseListener = () => void;
+
 class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
+  private closeListener?: OnCloseListener;
   constructor() {
     super(`<li class="page-item">
             <section class="page-item__body"></section>
@@ -10,10 +13,18 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
               <button class="close">&times;</button>
             </div>
           </li>`);
+    const closeBtn = this.element.querySelector('.close')! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      // closeListener가 있다면 closeListener() 실행
+      this.closeListener && this.closeListener();
+    };
   }
   addChild(child: Component) {
     const container = this.element.querySelector('.page-item__body')! as HTMLElement;
     child.attachTo(container);
+  }
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
   }
 }
 export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable {
@@ -26,5 +37,8 @@ export class PageComponent extends BaseComponent<HTMLUListElement> implements Co
     const item = new PageItemComponent();
     item.addChild(section);
     item.attachTo(this.element, 'beforeend');
+    item.setOnCloseListener(() => {
+      item.removeFrom(this.element);
+    });
   }
 }
